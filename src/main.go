0204d7df6997c54189b1b676414894ba624832c1
xcy0 <xcy2020@gmail.com>
2021-06-09 13:25:05 +0000

    chore grilla/mux

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Gorilla!\n"))
+}
 
 func main() {
-	fmt.Println("hello")
+    r := mux.NewRouter()
+    // Routes consist of a path and a handler function.
+    r.HandleFunc("/", YourHandler)
+
+    // Bind to a port and pass our router in
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
